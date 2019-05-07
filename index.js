201980e2a676f7a240ad2b21546d972489ea05ea
wangxiaoxiao <wangxiaoxiao@xuetangx.com>
2019-05-07 18:21:36 +0800

    xin

--- index.js
+++ index.js
@@ -2,10 +2,10 @@ let t=require('babel-types')
 
 
 let visitor={
-  ImportDeclaration(path){
+  ImportDeclaration(path,ref={opts:{}}){
     let node=path.node
     let specifiers=node.specifiers
-    if(t.isImportSpecifier(specifiers[0])){
+    if(ref.opts.libname===node.source.value&&t.isImportSpecifier(specifiers[0])){
       let result=specifiers.map(item=>
         (t.importDeclaration([t.importDefaultSpecifier(item.local)],
           t.stringLiteral(`${node.source.value}/${item.local.name}`)))
